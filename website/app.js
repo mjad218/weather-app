8a4504d85db49f9958b35f3f46b2b88bd5309af7
Mohamed Gad <mohamedgad149@gmail.com>
2021-08-31 22:55:28 +0200

    fixed incorrect zip code and displayed city not found

--- website/app.js
+++ website/app.js
@@ -4,30 +4,38 @@ const feelings = document.getElementById("feelings");
 const apiKey = "5260038d686b4334ff41dfddc7607c1d" ; 
 
 
-let displayData = () => {
+let displayData = ( err = 0) => {
+    let puplishDate = document.getElementById("date"); 
+    let content = document.getElementById("content"); 
+    let temp = document.getElementById("temp"); 
 
-    console.log("Display Started ! "); 
-    fetch("/data" , {
-        method: 'GET', 
-        headers: {
-            'Content-Type': 'application/json',
-        },
-      }    
-    ).then( (res) => {
-        return res.json();
-    })
-    .then( (res) => {
-        console.log("Data is Ready to be displayed ! ");
-        console.log(res);
-        let puplishDate = document.getElementById("date"); 
-        let content = document.getElementById("content"); 
-        let temp = document.getElementById("temp"); 
+    if( err == 0) {
+        // console.log("Display Started ! "); 
+        fetch("/data" , {
+            method: 'GET', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+          }    
+        ).then( (res) => {
+            return res.json();
+        })
+        .then( (res) => {
+            // console.log("Data is Ready to be displayed ! ");
+            // console.log(res);
+            temp.textContent = "Your city's temperature is " + res.Temperature + " °C"; 
+            content.textContent = "And your are feeling : " + res.Feeling; 
+            puplishDate.textContent = "Requested on " + res.PublishDate; 
+        } ) 
+        .catch ( (e) => {console.log(e)} ) ;
+    
+    } else {
 
-        temp.textContent = "Your city's temperature is " + res.Temperature + " °C"; 
-        content.textContent = "And your are feeling : " + res.Feeling; 
-        puplishDate.textContent = "Requested on " + res.PublishDate; 
-    } ) 
-    .catch ( (e) => {console.log(e)} ) ;
+        temp.textContent = "City Not Found!! "; 
+        content.textContent = "" ; 
+        puplishDate.textContent = ""; 
+
+    }
 }
 
 let postData = async (res , feeling) => {
@@ -40,7 +48,7 @@ let postData = async (res , feeling) => {
         Temperature : res.main.temp, 
         PublishDate : newDate
     }; 
-    console.log("Post Started ! "); 
+    // console.log("Post Started ! "); 
 
     let responsePost = await fetch("/data" , {
         method: 'POST', 
@@ -60,21 +68,32 @@ const generateData = async () => {
 
     const zipCode = zip.value; 
     const APIURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=metric&appid=${apiKey}` ;
+
     await fetch(APIURL)
-    .then( (response) => {
-        return response.json(); 
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("City not found")
+        } else {
+            return response.json(); 
+        }
+
     })
     .then((response) => {
-        console.log(response); 
+        // console.log(response); 
 
         postData(response,feelings.value);
+        displayData() ;
+
         })
     .catch( (e) => {
-        console.log(e); 
-    }) ;
-    console.log("Post Done ! "); 
+        // console.log(e); 
+        displayData(1) ;
 
-    displayData() ;
+    });
+
+    
+    // console.log("Post Done ! "); 
+
 
 }
 
